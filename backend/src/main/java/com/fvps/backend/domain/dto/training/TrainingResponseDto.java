249d09616dee6afa7de3a5d36588c20cd5b44933
mihlsap <mgrzyb527@gmail.com>
2025-12-12 09:44:37 +0100

    modified TrainingResponseDto

---
+++ backend/src/main/java/com/fvps/backend/domain/dto/training/TrainingResponseDto.java
@@ -0,0 +1,21 @@
+package com.fvps.backend.domain.dto.training;
+
+import com.fvps.backend.domain.enums.TrainingType;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+public class TrainingResponseDto {
+    private UUID id;
+    private String title;
+    private String description;
+    private TrainingType type;
+    private int validityPeriodDays;
+    private Double passingThreshold;
+    private List<ModuleDto> modules;
+    private Long version;
+}
