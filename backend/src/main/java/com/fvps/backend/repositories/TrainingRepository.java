fff7ea49dbe4c692e74c691029896bf6ead674ea
mihlsap <mgrzyb527@gmail.com>
2025-12-12 10:17:11 +0100

    modified TrainingRepository

--- backend/src/main/java/com/fvps/backend/repositories/TrainingRepository.java
+++ backend/src/main/java/com/fvps/backend/repositories/TrainingRepository.java
@@ -2,6 +2,7 @@ package com.fvps.backend.repositories;
 
 import com.fvps.backend.domain.entities.Training;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.UUID;
 
 public interface TrainingRepository extends JpaRepository<Training, UUID> {
