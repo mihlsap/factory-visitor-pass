02ca713355616bde0928e457fd42362b24dd4fde
mihlsap <mgrzyb527@gmail.com>
2025-12-12 09:45:38 +0100

    modified UserSummaryDto

--- backend/src/main/java/com/fvps/backend/domain/dto/user/UserSummaryDto.java
+++ backend/src/main/java/com/fvps/backend/domain/dto/user/UserSummaryDto.java
@@ -1,4 +1,27 @@
 package com.fvps.backend.domain.dto.user;
 
+import com.fvps.backend.domain.enums.UserRole;
+import com.fvps.backend.domain.enums.UserStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserSummaryDto {
+    private UUID id;
+    private String email;
+    private String name;
+    private String surname;
+    private String companyName;
+    private UserRole role;
+    private UserStatus status;
+    private String photoUrl;
+    private String phoneNumber;
+    private Long version;
 }
