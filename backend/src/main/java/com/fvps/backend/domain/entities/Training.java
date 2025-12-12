d341627b1a3c4cf76a70c6cea824d044deec608b
mihlsap <mgrzyb527@gmail.com>
2025-12-12 09:41:59 +0100

    modified Training entity

--- backend/src/main/java/com/fvps/backend/domain/entities/Training.java
+++ backend/src/main/java/com/fvps/backend/domain/entities/Training.java
@@ -4,12 +4,16 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fvps.backend.domain.enums.TrainingType;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
+@ToString
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -31,10 +35,34 @@ public class Training {
     @Enumerated(EnumType.STRING)
     private TrainingType type;
 
-    private int validityPeriodDays; // np. 365
+    private int validityPeriodDays;
+
+    @Builder.Default
+    private Double passingThreshold = 0.8;
+
+    @Version
+    @Column(nullable = false)
+    private Long version;
 
     @OneToMany(mappedBy = "training", cascade = CascadeType.ALL, orphanRemoval = true)
-    @OrderBy("orderIndex ASC") // Ważne: zawsze pobieraj moduły w dobrej kolejności
+    @OrderBy("orderIndex ASC")
     @Builder.Default
+    @ToString.Exclude
     private List<TrainingModule> modules = new ArrayList<>();
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Training training = (Training) o;
+        return getId() != null && Objects.equals(getId(), training.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
