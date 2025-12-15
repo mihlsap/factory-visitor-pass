d165ecb2079165c0e9304b0821d225392c5fa931
mihlsap <mgrzyb527@gmail.com>
2025-12-15 13:18:28 +0100

    added AppConfig class

---
+++ backend/src/main/java/com/fvps/backend/config/AppConfig.java
@@ -0,0 +1,40 @@
+package com.fvps.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.time.Clock;
+import java.util.Locale;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemDefaultZone();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+    @Bean
+    public Locale defaultLocale(@Value("${app.messages.locale}") String localeCode) {
+        return Locale.forLanguageTag(localeCode);
+    }
+}
