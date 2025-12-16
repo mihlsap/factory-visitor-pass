c4a30be3d3b37a4c20896d9b14f6eb61111a7502
mihlsap <mgrzyb527@gmail.com>
2025-12-16 13:31:39 +0100

    added API documentation for UserTrainingDto

--- backend/src/main/java/com/fvps/backend/domain/dto/training/UserTrainingDto.java
+++ backend/src/main/java/com/fvps/backend/domain/dto/training/UserTrainingDto.java
@@ -1,7 +1,7 @@
 package com.fvps.backend.domain.dto.training;
 
-import com.fvps.backend.domain.entities.Training;
 import com.fvps.backend.domain.enums.ProgressStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,12 +15,28 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserTrainingDto {
-    private UUID id; // ID przypisania (UserTrainingStatus)
-    private Training training; // Pełne dane szkolenia
+
+    @Schema(description = "UserTrainingStatus ID", example = "b123...")
+    private UUID id;
+
+    @Schema(description = "Training details")
+    private TrainingSummaryDto training;
+
+    @Schema(description = "Current progress status", example = "IN_PROGRESS")
     private ProgressStatus status;
+
+    @Schema(description = "Index of the current active module", example = "2")
     private int currentModuleIndex;
+
+    @Schema(description = "Quiz score (if completed)", example = "0.9")
     private Double quizScore;
+
+    @Schema(description = "Completion timestamp", example = "2023-10-25T14:30:00")
     private LocalDateTime completedAt;
+
+    @Schema(description = "Training validity expiration date", example = "2024-10-25T14:30:00")
     private LocalDateTime validUntil;
+
+    @Schema(description = "Indicates if the training was manually revoked by admin", example = "false")
     private boolean isPassRevoked;
 }
