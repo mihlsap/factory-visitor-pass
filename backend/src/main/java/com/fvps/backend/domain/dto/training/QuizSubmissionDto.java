fc5bac149d34937d83dae95f614098085053937f
mihlsap <mgrzyb527@gmail.com>
2025-12-16 13:31:12 +0100

    added API documentation for QuizSubmissionDto

--- backend/src/main/java/com/fvps/backend/domain/dto/training/QuizSubmissionDto.java
+++ backend/src/main/java/com/fvps/backend/domain/dto/training/QuizSubmissionDto.java
@@ -1,14 +1,21 @@
 package com.fvps.backend.domain.dto.training;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class QuizSubmissionDto {
-    private List<Integer> answers;
+
+    @Schema(
+            description = "Map of answers where Key is Question UUID and Value is selected Option Index",
+            example = "{\"770e8400-e29b-41d4-a716-446655440099\": 1, \"880e8400-...\": 0}"
+    )
+    private Map<UUID, Integer> answers;
 }
