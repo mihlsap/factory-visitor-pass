671a7357b41fe90aefe9db6f970a7cd04c7f209d
mihlsap <mgrzyb527@gmail.com>
2026-02-13 17:55:06 +0100

    updated CreateTrainingRequest

--- backend/src/main/java/com/fvps/backend/domain/dto/training/CreateTrainingRequest.java
+++ backend/src/main/java/com/fvps/backend/domain/dto/training/CreateTrainingRequest.java
@@ -10,19 +10,35 @@ import java.util.List;
 @Data
 public class CreateTrainingRequest {
 
-    @Schema(description = "Training title", example = "OHS Safety - Level 1", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(
+            description = "Training title",
+            example = "OHS Safety - Level 1",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     @NotBlank(message = "Title is required.")
     private String title;
 
-    @Schema(description = "Detailed description of the training content", example = "Basic safety rules for factory visitors.", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(
+            description = "Detailed description of the training content",
+            example = "Basic safety rules for factory visitors.",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     @NotBlank(message = "Description is required.")
     private String description;
 
-    @Schema(description = "Type/Category of the training", example = "OHS", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Schema(
+            description = "Type/Category of the training",
+            example = "OHS",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     @NotNull(message = "Type of training is required.")
     private TrainingType type;
 
-    @Schema(description = "Number of days the training is valid after completion", example = "365", minimum = "1")
+    @Schema(
+            description = "Number of days the training is valid after completion",
+            example = "365",
+            minimum = "1"
+    )
     @Min(value = 1, message = "Validity period has to be at least one day.")
     private int validityPeriodDays;
 
@@ -37,11 +53,19 @@ public class CreateTrainingRequest {
     @Schema(description = "Optimistic locking version (optional for creation)", example = "1")
     private Long version;
 
-    @Schema(description = "Security clearance level granted upon completion (1-4)", example = "1", minimum = "1", maximum = "4")
+    @Schema(
+            description = "Security clearance level granted upon completion (1-4)",
+            example = "2",
+            minimum = "1",
+            maximum = "4"
+    )
     @Min(value = 1, message = "Security level must be at least 1")
     @Max(value = 4, message = "Security level cannot be higher than 4")
     private int securityLevel;
 
     @Schema(description = "If true, forces users to retake the training even if completed", example = "false")
     private boolean resetProgress;
+
+    @Schema(description = "Excluded from score", example = "false")
+    private boolean excludedFromScore;
 }
